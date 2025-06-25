32c8968ff7159fc1ec21b94e590be1cbf047e9b7
aleksem07 <aleksem07@gmail.com>
2025-06-25 13:09:02 +0300

    [feat] add message if no find photo albums

--- src/components/PhotoTable.vue
+++ src/components/PhotoTable.vue
@@ -4,50 +4,61 @@
       @scroll="handleScroll"
       ref="tableContainer"
   >
-    <table class="w-full text-left text-sm table-fixed">
-      <thead class="bg-zinc-100 text-zinc-700 sticky top-0 z-10">
-      <tr>
-        <th
-            v-for="column in columns"
-            :key="column.key"
-            @click="() => emit('sort', column.key)"
-            class="cursor-pointer px-4 py-2 hover:bg-zinc-200 transition truncate whitespace-nowrap"
-            :style="{ width: column.width }"
-        >
-          {{ column.label }}
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr
-          v-for="photo in visiblePhotos"
-          :key="photo.id"
-          class="border-t hover:bg-zinc-50"
-      >
-        <td class="px-4 py-2 truncate whitespace-nowrap overflow-hidden">
-          {{ photo.id }}
-        </td>
-        <td class="px-4 py-2 truncate whitespace-nowrap overflow-hidden">
-          {{ photo.albumId }}
-        </td>
-        <td class="px-4 py-2 truncate whitespace-nowrap overflow-hidden]" :title="photo.title">
-          {{ photo.title }}
-        </td>
-        <td class="px-4 py-2 truncate whitespace-nowrap overflow-hidden" :title="photo.url">
-          <a
-              :href="photo.url"
-              target="_blank"
-              class="hover:underline"
+    <template v-if="visiblePhotos.length">
+      <table class="w-full text-left text-sm table-fixed">
+        <thead class="bg-zinc-100 text-zinc-700 sticky top-0 z-10">
+        <tr>
+          <th
+              v-for="column in columns"
+              :key="column.key"
+              @click="() => emit('sort', column.key)"
+              class="cursor-pointer px-2 py-2 text-center hover:bg-blue-600 hover:text-white  transition  whitespace-nowrap"
+              :style="{ width: column.width }"
           >
-            {{ photo.url }}
-          </a>
-        </td>
-        <td class="px-4 py-2 truncate whitespace-nowrap overflow-hidden" :title="photo.thumbnailUrl">
-          {{ photo.thumbnailUrl }}
-        </td>
-      </tr>
-      </tbody>
-    </table>
+            {{ column.label }}
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="photo in visiblePhotos"
+            :key="photo.id"
+            class="border-t hover:bg-blue-600 hover:text-white transition "
+        >
+          <td class="px-4 py-2 whitespace-nowrap">
+            {{ photo.id }}
+          </td>
+          <td class="px-4 py-2  whitespace-nowrap">
+            {{ photo.albumId }}
+          </td>
+          <td class="px-4 py-2 truncate whitespace-nowrap" :title="photo.title">
+            {{ photo.title }}
+          </td>
+          <td class="px-4 py-2 truncate whitespace-nowrap" :title="photo.url">
+            <a
+                :href="photo.url"
+                target="_blank"
+                class="hover:underline"
+            >
+              {{ photo.url }}
+            </a>
+          </td>
+          <td class="px-4 py-2 truncate whitespace-nowrap" :title="photo.thumbnailUrl">
+            <a
+                :href="photo.thumbnailUrl"
+                target="_blank"
+                class="hover:underline"
+            >
+            {{ photo.thumbnailUrl }}
+            </a>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </template>
+
+    <p v-else class="p-4 text-center text-zinc-500">Альбомы не найдены</p>
+
   </div>
 </template>
 
@@ -65,10 +76,10 @@ const emit = defineEmits(['sort'])
 
 const columns = [
   { key: 'id', label: 'ID', width: '10%' },
-  { key: 'albumId', label: 'Альбом', width: '10%' },
+  { key: 'albumId', label: 'Альбом', width: '15%' },
   { key: 'title', label: 'Название', width: '25%' },
   { key: 'url', label: 'Ссылка', width: '25%' },
-  { key: 'thumbnailUrl', label: 'Миниатюра', width: '30%' }
+  { key: 'thumbnailUrl', label: 'Миниатюра', width: '25%' }
 ]
 
 const LIMIT = 30
